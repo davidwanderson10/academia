126e5d4eb0ab078755ebc476563246457e7ca017
davidwanderson10 <profdavidwanderson@gmail.com>
2026-03-30 21:09:27 -0300

    criando pagina academia

---
+++ script.js
@@ -0,0 +1,28 @@
+let currentSlide = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    slides.style.transform = `translateX(-${currentSlide * 100 / 3}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Auto slide every 3 seconds
+setInterval(nextSlide, 3000);
+
+// Initial display
+showSlide(currentSlide);
